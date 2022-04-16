a5e47f602455029f4519014e60676ce249dab699
Tran Thuy <thuytt0295@gmail.com>
2022-04-16 21:57:38 +0700

    render all days in html

--- mooth-calendar/main.js
+++ mooth-calendar/main.js
@@ -1,26 +1,37 @@
-function getTotalDays(year, month) {
-	return new Date(year, month, 0).getDate();
-}
-
 const currentYear = new Date().getFullYear();
 
 const months = new Array(12).fill().map((_, index) => {
 	const firstDate = new Date(currentYear, index, 1);
 	const monthName = firstDate.toLocaleString('en-us', { month: 'long' });
-	const firstDateName = firstDate.toLocaleString('en-us', {
-		weekday: 'short'
-	});
-
-	console.log(firstDate);
-
 	return {
 		name: monthName,
-		totalDays: getTotalDays(currentYear, index + 1),
-		firstDate: firstDate.getDay(),
-		month: firstDate.getMonth(),
-		firstDateName
 	};
 });
+
+function addDays(date, days) {
+	const currentDate = new Date(date);
+	currentDate.setDate(currentDate.getDate() + days);
+	return currentDate;
+}
+
+function getAllDays (year){
+	const firstDateOfYear = new Date(year, 1,1);
+	const lastDateOfYear = new Date(year, 12, 31);
+
+	const dates = [firstDateOfYear];
+	let lastDate = firstDateOfYear;
+
+	while(lastDate.getTime() !== lastDateOfYear.getTime()){
+		dates.push(addDays(lastDate, 1));
+		lastDate = dates[dates.length -1];
+
+	}
+
+	return dates;
+}
+
+
+const dateList = getAllDays(currentYear);
 
 const yearElement = document.getElementById('currentYear');
 const calendar = document.getElementById('calendar');
@@ -29,33 +40,50 @@ yearElement.innerHTML = new Date().getFullYear();
 
 let calendarHTML = '';
 months.map((item, index) => {
-	console.log(item.firstDate);
-	let listDate = '';
-	for (let i = 1; i <= item.totalDays; i++) {
-		const date = new Date(currentYear, item.month, i).getDate();
-
-		// console.log(date);
-		// if(i < 7 && )
-	}
-
 	const monthContainer = document.createElement('div');
-	monthContainer.classList.add('month');
-	monthContainer.innerHTML = `<div class="month">
-	<h4 class="month__name">${item.name}</h4>
-	<div class='days ${item.name}_days'>
-	<div class="days__list">
-	<span>Sun</span>
-	<span>Mon</span>
-	<span>Tue</span>
-	<span>Wed</span>
-	<span>Thu</span>
-	<span>Fri</span>
-	<span>Sat</span>
-</div>
-	</div>
+	monthContainer.classList.add('month')
+	monthContainer.innerHTML = `<h4 class="month__name">${item.name}</h4>
+	<div class='days month-${index + 1} '>
+		<div class="days__list month-${index + 1} ">
+		<div class="date"><span class="circle-text">Sun</span></div>
+		<div class="date"><span class="circle-text">Mon</span></div>
+		<div class="date"><span class="circle-text">Tue</span></div>
+		<div class="date"><span class="circle-text">Wed</span></div>
+		<div class="date"><span class="circle-text">Thu</span></div>
+		<div class="date"><span class="circle-text">Fri</span></div>
+		<div class="date"><span class="circle-text">Sat</span></div>
+		</div>
 	</div>`;
 
 	calendar.appendChild(monthContainer);
 });
 
+dateList.forEach(item => {
+	const month = item.getMonth();
+	const month_container = document.querySelector(`.days__list.month-${month + 1}`)
+
+	if(item.getDate() === 1 && item.getDay() !== 0){
+		for(let i = 0; i < item.getDay(); i++){
+			const emptyEl = document.createElement('div');
+			emptyEl.classList.add('date');
+			emptyEl.innerHTML = '<span class="circle-text"></span>';
+			month_container.appendChild(emptyEl)
+		}
+
+	}
+
+	const existDateEl = document.createElement('div');
+	existDateEl.classList.add('date');
+	existDateEl.innerHTML = `<span class="circle">${item.getDate()}</span>`
+
+	month_container.appendChild(existDateEl)
+
+
+
+})
+
+
+
 // calendar.innerHTML = calendarHTML;
+
+
