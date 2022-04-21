2e534d944ec7b75cee93e84e7cf1b7b835654cf1
Tran Thuy <thuytt0295@gmail.com>
2022-04-21 23:37:39 +0700

    complete generate password

--- password-generator/main.js
+++ password-generator/main.js
@@ -1,32 +1,64 @@
-const passwordElement = document.getElementById('password');
+const passwordElement = document.getElementsByClassName('password')[0];
 const btnCopy = document.querySelector('.btn-copy');
-const inputElements = document.querySelectorAll('input[type="checkbox"]');
+const inputElements = document.querySelectorAll(
+	'input[type="checkbox"]'
+);
 const generatorBtn = document.getElementById('generator');
 const inputNumberElement = document.querySelector('.number-password');
 
-const numbers = new Array(10).fill(1).map((_, index) => index);
-const upperCaseLetters = new Array(26)
+const number = new Array(10).fill(1).map((_, index) => index);
+const uppercase = new Array(26)
 	.fill(65)
-	.map((item, index) =>  String.fromCharCode(item + index));
+	.map((item, index) => String.fromCharCode(item + index));
 
-const lowercaseLetters = upperCaseLetters.map(item => item.toLocaleLowerCase())
+const symbol = ["$", "@", "#", "%"];
+
+const lowercase = uppercase.map(item => item.toLocaleLowerCase());
 const lengthPassword = inputNumberElement.value;
 
-console.log(lengthPassword)
+function setLengthOfPassword() {
+	const length = inputNumberElement.value
+		? parseInt(inputNumberElement.value)
+		: 0;
 
-function setLengthOfPassword () {
-	inputNumberElement.addEventListener('change', function(e){
-		const {value} = e.target;
-		if(value) return parseInt(value);
-		return 0;
-	})
-
-	return 0;
+	return length;
 }
-
-
 
 generatorBtn.addEventListener('click', () => {
 	console.log('click button');
 	const length = setLengthOfPassword();
+	const pattern = Array.from(inputElements).filter(item => item.checked).map(item => item.getAttribute("id"));
+	console.log(pattern);
+
+
+	if (!length || !pattern.length) {
+		passwordElement.innerHTML = '';
+	} else {
+		let password = '';
+		const allCharacters = pattern.reduce((res, current) => {
+			if(current === 'number') return [...res, ...number];
+			if(current === 'uppercase') return [...res, ...uppercase];
+			if(current === 'lowercase') return [...res, ...lowercase];
+			if(current === 'symbol') return [...res, ...symbol];
+			return res;
+		}, []);
+
+		const lengthChar = allCharacters.length;
+		for(let i = 0; i < length ; i++){
+			const randomIndex = Math.floor(Math.random() * lengthChar);
+			password += allCharacters[randomIndex];
+
+		}
+
+		passwordElement.innerHTML = password;
+
+		console.log(allCharacters);
+
+	}
+});
+
+btnCopy.addEventListener('click', function(e){
+	navigator.clipboard.writeText(passwordElement.innerHTML);
+
 })
+
