53f94da83c02d56fe5c562667cc4a063f9744349
Tran Thuy <thuytt0295@gmail.com>
2022-04-29 06:49:45 +0700

    add project gravity switch

---
+++ gravity-switch/main.js
@@ -0,0 +1,5 @@
+const switchContainer = document.getElementsByClassName('switch-container')[0];
+const ball = document.getElementsByClassName('ball')[0];
+
+console.log(switchContainer);
+console.log(ball)
