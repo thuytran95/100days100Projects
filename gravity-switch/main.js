c36f9a832e26c9e6b5351a5d06dfdf387e567131
Tran Thuy <thuytt0295@gmail.com>
2022-05-10 21:27:55 +0700

    restyling animation for runing ball:

--- gravity-switch/main.js
+++ gravity-switch/main.js
@@ -1,19 +1,17 @@
 const switchContainer = document.getElementsByClassName('switch-container')[0];
 const ball = document.getElementsByClassName('ball')[0];
 
-switchContainer.addEventListener("click", ()=>{
-	switchContainer.classList.toggle('down');
+switchContainer.addEventListener('click', () => {
 	const isDown = switchContainer.classList.contains('down');
 	const isUp = switchContainer.classList.contains('up');
 
-	console.log(isDown);
-	if(isDown){
-		switchContainer.classList.remove('down');
+	if (isDown) {
 		switchContainer.classList.add('up');
+		switchContainer.classList.remove('down');
 	}
 
-	if(isUp){
-		switchContainer.classList.remove('up');
+	if (isUp) {
 		switchContainer.classList.add('down');
+		switchContainer.classList.remove('up');
 	}
-})
+});
