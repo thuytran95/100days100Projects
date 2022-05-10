6396931fcf997fbda57b0ccfc51bcc485556e037
Tran Thuy <thuytt0295@gmail.com>
2022-05-10 07:12:44 +0700

    update animation for ball rolls

--- gravity-switch/main.js
+++ gravity-switch/main.js
@@ -1,5 +1,19 @@
 const switchContainer = document.getElementsByClassName('switch-container')[0];
 const ball = document.getElementsByClassName('ball')[0];
 
-console.log(switchContainer);
-console.log(ball)
+switchContainer.addEventListener("click", ()=>{
+	switchContainer.classList.toggle('down');
+	const isDown = switchContainer.classList.contains('down');
+	const isUp = switchContainer.classList.contains('up');
+
+	console.log(isDown);
+	if(isDown){
+		switchContainer.classList.remove('down');
+		switchContainer.classList.add('up');
+	}
+
+	if(isUp){
+		switchContainer.classList.remove('up');
+		switchContainer.classList.add('down');
+	}
+})
